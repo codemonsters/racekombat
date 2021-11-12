0fd17651a7f0904d6937299da92b9bfaa1920da7
Ypablosv5Y <pabloseova@gmail.com>
2021-11-12 19:57:01 +0100

    Instanciado héroe

--- server/KinematicBody2D.gd
+++ server/KinematicBody2D.gd
@@ -5,3 +5,11 @@ var a = Vector2(0,1)
 
 func _process(delta):
 	move_and_collide(a)
+	if Input.is_action_pressed("ui_right"):
+		move_local_x(1)
+	if Input.is_action_pressed("ui_left"):
+		move_local_x(-1)
+	if Input.is_action_pressed("ui_up"):
+		move_local_y()
+	#if Input.is_action_pressed("ui_down"):
+		#move_local_y(-1)
--- server/TileMap.gd
+++ server/TileMap.gd
@@ -13,8 +13,21 @@ func create_noise(x_pos,y_pos,width,height):
 				set_cell(01*x+x_pos,01*y+y_pos,randi()%2+0)
 			else:
 				set_cell(01*x+x_pos,01*y+y_pos,2)
+				
+#func create_map(cn_x,cn_y,width,height):
+#	for cn_x in range(width):
+#		for cn_y in range(height):
+#			var local_width = 5
+#			local_width = randi()%5+0
+#			var local_height = 5
+#			local_height = randi()%4+1
+#			create_noise(cn_x+local_width,cn_y+local_height,local_width,local_height)
 
 func _ready():
-	create_noise(0,0,20,5)
+#	create_map(-40,0,100,10)
+	create_noise(0,0,50,5)
+	create_noise(-40,0,20,5)
+	self.position.x = (ProjectSettings.get("display/window/size/width"))/2
+	self.position.y = (ProjectSettings.get("display/window/size/height"))/2
 #noise.get_noise_2d(x+x_pos,y+y_pos)
 
