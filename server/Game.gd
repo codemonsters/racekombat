43fd09981ea0e0357ed5a6b548f873102a370c61
yagueto <yagofreirefdez@gmail.com>
2021-11-19 18:51:02 +0100

    Soluciona error al cambiar el nombre de un archivo :S

--- server/Game.gd
+++ server/Game.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 
+var controllerManager: ControllersManager
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# ControllersManager.new()
+	controllerManager = ControllersManager.new()
 	# Conecta la señal con la función. La hay que asignar a una variable pq sino 
 	# lanza un warning :S
 	var _im_just_trash = $Menu.connect("change_to_saladeespera", self,
@@ -13,5 +16,5 @@ func _ready():
 func change_to_saladeespera():
 	get_node("Menu").queue_free()
 	var screen_node = get_parent()
-	var sala_de_espera = preload("res://SaladeEspera.tscn")
+	var sala_de_espera = preload("res://WaitingRoom.tscn")
 	screen_node.add_child(sala_de_espera.instance())
