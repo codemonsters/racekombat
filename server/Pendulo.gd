8127c21886d3437d97c3e119279b81125801d46e
Braisa <oterolemabrais@gmail.com>
2021-12-07 01:43:10 +0100

    Velocidad del péndulo en función del ángulo

--- server/Pendulo.gd
+++ server/Pendulo.gd
@@ -1,31 +1,34 @@
 extends Node2D
 
-# ARREGLAR POSICION INICIAL
-# SUAVIZAR MOVIMIENTO
-
 var amplitud
-export var radio = Vector2(300, 100)
+export var radio = Vector2(100, 100)
 export var ancho_plataforma = 100
-var posicion_inicial = radio
+export var izquierda = false
 var angulo
 var k = 1
+var v = 1
 onready var plataforma = get_node("Plataforma")
 
 func _ready():
 	amplitud = PI + 2*(radio.angle_to(Vector2.RIGHT))
-	angulo = 0
-	posicion_inicial = Vector2(600, 400)
+	if izquierda:
+		angulo = amplitud
+	else:
+		angulo = 0
 	plataforma.get_node("ColorRect").rect_size = Vector2(ancho_plataforma, 20)
-	plataforma.position = posicion_inicial - Vector2(ancho_plataforma/2, 0)
+	plataforma.get_node("CollisionShape2D").shape.extents = Vector2(ancho_plataforma/2, 10)
+	plataforma.get_node("CollisionShape2D").position = Vector2(ancho_plataforma/2, 10)
+	plataforma.position = -Vector2(ancho_plataforma/2, 0)
 
 func _process(delta):
 	if angulo >= amplitud:
 		k = -1
 	elif angulo <= 0:
 		k = 1
-	angulo += delta * k
-	plataforma.position = posicion_inicial + radio.rotated(angulo) - Vector2(ancho_plataforma/2, 0)
-	update()
+	v = 1.25 - (abs((amplitud/2) - angulo)/(amplitud/2))
+	angulo += delta * k * v
+	plataforma.position = radio.rotated(angulo) - Vector2(ancho_plataforma/2, 0)
+	update() # Para llamar a _draw()
 
 func _draw():
-	draw_line(posicion_inicial, plataforma.position + Vector2(ancho_plataforma/2, 10), Color(255, 0, 0), 5)
+	draw_line(Vector2(0,0), plataforma.position + Vector2(ancho_plataforma/2, 10), Color(255, 0, 0), 5)
