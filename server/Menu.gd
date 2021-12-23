597b3f9b2be999a697bb53c29669f9a99e4ab0b3
Autotren <mail@uino.eu>
2021-12-23 17:51:45 +0100

    Añadida animación al menu

--- server/Menu.gd
+++ server/Menu.gd
@@ -4,6 +4,9 @@ signal change_to_saladeespera
 
 var active_button = 0
 
+func _ready():
+	var lbl := $Label
+	_move_text(lbl)
 
 # TODO: ¿Por qué detecta "action" venga del controller que venga, y a continuación cambia de escena?
 # ¿Podría tener que ver con el focus?
@@ -30,3 +33,15 @@ func controller_input(_controller, action, is_main):
 			"action":
 				$Control.get_children()[active_button].set_pressed(true)
 				$Control.get_children()[active_button].emit_signal("pressed")
+
+func _move_text(label):
+	var t = Timer.new()
+	t.set_wait_time(0.03)
+	t.set_one_shot(true)
+	self.add_child(t)
+	var accel = Vector2(50, 0)
+	for _a in range(1, 41.88184959941402): # x = x0 + v0 * t + 1/2 * a * t^2
+		label.rect_global_position += accel
+		accel -= Vector2(1.2, 0)
+		t.start()
+		yield(t, "timeout")
