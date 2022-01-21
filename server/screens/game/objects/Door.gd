b60db12b45586d27b10ecf43a84bfc715ed9b372
yagueto <yagofreirefdez@gmail.com>
2022-01-21 19:17:11 +0100

    Corrige problemas con puerta (algunos)

--- server/screens/game/objects/Door.gd
+++ server/screens/game/objects/Door.gd
@@ -7,14 +7,14 @@ func open():
 	if open:
 		return
 
-	$Tween.interpolate_property(self, "position:y", null, position.y -70, 1.5, Tween.TRANS_LINEAR)
+	$Tween.interpolate_property(self, "position:y", null, 510, 1.5, Tween.TRANS_QUINT)
 	$Tween.start()
 
 func close():
 	if not open:
 		return
 
-	$Tween.interpolate_property(self, "position:y", null, position.y + 70, 1.5, Tween.TRANS_LINEAR)
+	$Tween.interpolate_property(self, "position:y", null, 578, 1.5, Tween.TRANS_QUINT)
 	$Tween.start()
 
 
