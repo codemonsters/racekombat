aa66069d957776481b1e9473c3c85e4772602aec
Braisa <oterolemabrais@gmail.com>
2022-01-21 20:00:28 +0100

    Creando huecos

--- server/screens/game/Gen_Tilemap2_0.gd
+++ server/screens/game/Gen_Tilemap2_0.gd
@@ -4,14 +4,16 @@ var noise = OpenSimplexNoise.new()
 
 func _ready():
 	randomize()
-	_create_suelo(100,5,-50,0)
+	
+	_create_suelo(1000,5,-50,0)
+	_create_hueco(1000,5,-50,0)
+	#_create_rampa(100,5,0,10)
 	
 func _create_suelo(width,height,pos_x,pos_y):
 	
-	noise.seed = randi()
-	noise.octaves = 1
-	noise.period = 1000
-	noise.persistence = 0.5
+	noise.octaves = 0
+	noise.period = 100000
+	noise.lacunarity= 1000
 	
 	for x in range(width):
 		for y in range(height):
@@ -22,50 +24,24 @@ func _create_suelo(width,height,pos_x,pos_y):
 				
 	$TileMap.update_bitmask_region()
 	
-#	randomize()
-#	var length = 10
-#	var width = 50
-#	var distance = 0
-#	var total_distance = 0
-#
-#	for x in range(length):
-#		total_distance += distance
-#		_create_noise(width, 30, x * width + total_distance, 10)
-#		var tile = $TileMap.world_to_map(Vector2(width * x + total_distance, 30))
-#		distance = randi()%10+1
-#		print(tile)
-#		#if rand_range(1, 10) > 8:
-#			#distance = 20
-#	_create_noise_roof(width * length + total_distance, 20, 0, -3)
-#
-#
-#
-#
-#func _create_noise(width,height,pos_x,pos_y):
-#	noise.seed = randi()
-#	noise.octaves = 20
-#	noise.period = 15.0 #rand_range(15.0, 30.0)
-#	noise.persistence = 0.8
-#
-#
-#	for x in range(width):
-#		for y in range(height):
-#			var noise_height = noise.get_noise_1d(x)
-#
-#			if ceil(noise_height * 25) < y :
-#				$TileMap.set_cellv(Vector2(pos_x+x, pos_y+y), 2)
-#
-#func _create_noise_roof(width, height, pos_x, pos_y):
-#	noise.seed = randi()
-#	noise.octaves = 8
-#	noise.period = 10
-#	noise.persistence = 0.5
-#	for x in range(width):
-#		for y in range(height):
-#			var noise_height = noise.get_noise_1d(x)
-#
-#			if ceil(noise_height * 40) < y :
-#				$RoofTileMap.set_cellv(Vector2(pos_x+x, pos_y+y), 0)
-#
-#	$RoofTileMap.scale.y = -1
-#	$RoofTileMap.update_bitmask_region()
+func _create_hueco(width,height,pos_x,pos_y):
+	var distance_entre_huecos = 0
+	var width_huecos = 0
+	distance_entre_huecos = randi()%10+10
+	width_huecos = randi()%5+5
+	for x in range(width):
+		for y in range(height):
+			if (pos_x+x) == 0:
+				pass
+			elif (pos_x+x)%distance_entre_huecos == 0:
+				#for z in range(width_huecos):
+				$TileMap.set_cellv(Vector2(pos_x+x, pos_y+y), 1)
+	
+	
+func _create_rampa(width,height,pos_x,pos_y):
+	for x in range(width):
+		for y in range(height):
+			$TileMap.set_cellv(Vector2(pos_x+x, pos_y+y), 2)
+	
+	$TileMap.update_bitmask_region()
+
