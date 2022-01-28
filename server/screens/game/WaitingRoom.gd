dd852a4bedc333ddf60925d737d00f12aac99f1a
yagueto <yagofreirefdez@gmail.com>
2022-01-28 19:55:57 +0100

    Arreglada issue #19 Ahora solo se crean jugadores al soltar el botón "acción". Reescalado el tamaño del jugador para que sea más grande.

--- server/screens/game/WaitingRoom.gd
+++ server/screens/game/WaitingRoom.gd
@@ -7,22 +7,23 @@ var bodies_in_door = []
 const PlayerResource = preload("res://screens/game/player/Player.tscn")
 
 func controller_input(_controller, action, _is_main, is_pressed):
-	var found := false
-	var controller_index : int
+	var player_found := false
+	var player_index : int # Posición del jugador en la lista
 	for i in players.size():
 		if _controller in players[i].keys():
-			found = true
-			controller_index = i
-	if not found:
+			player_found = true
+			player_index = i
+			break
+	#
+	if player_found: # Si el controlador tiene un jugador asignado, le pasamos su input
+		var active_player = players[player_index][_controller]
+		active_player._handle_input(action, is_pressed)
+	elif action == "action" and not is_pressed: # Si el controlador no tiene un jugador asignado y se pulsa y se suelta "acción", le asignamos el jugador
 		var new_player = PlayerResource.instance()
 		new_player.position = Vector2(300, 300)
 		add_child(new_player)
 		players.append({_controller: new_player})
 		PlayerAppear.PlayerSpawnSound()
-	else:
-		var active_player = players[controller_index][_controller]
-		active_player._handle_input(action, is_pressed)
-
 
 
 func _on_Limite_body_entered(body):
