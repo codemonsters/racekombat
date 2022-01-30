c129af4f89acfc30f249c245df00878a1574c7f2
Autotren <mail@uino.eu>
2022-01-30 01:14:20 +0100

    Arreglado problema causado por el envío de señales duplicadas de parte del cliente, rompiendo el funcionamiento de player.

--- server/utils/GamePadController.gd
+++ server/utils/GamePadController.gd
@@ -2,13 +2,14 @@ class_name GamePadController extends Controller
 
 var _controllersManager: ControllersManager
 var _id : int
+var states : Array
 
 
 
 func _init(controllersManager: ControllersManager, id: int):
 	_controllersManager = controllersManager
 	_id = id
-
+	states = ["left", false, "right", false, "up", false, "down", false, "action", false]
 
 func _to_string():
 	return (
@@ -18,7 +19,11 @@ func _to_string():
 func input(event:String, is_pressed:bool):
 	if event == "A":
 		event = "action"
-	_controllersManager.controller_input(self, event, is_pressed)
+	var index = states.find(event)
+	if not bool(states[index + 1]) == is_pressed:
+		_controllersManager.controller_input(self, event, is_pressed)
+		states[index + 1] = is_pressed
+		print("Event: " + event + " is " + str(is_pressed) + " executed.")
 	return true
 
 func get_id():
