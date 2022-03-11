7150f2f6fcd2c27f5cc6fda259aa52be7ee9ba51
Yagueto <48566793+yagueto@users.noreply.github.com>
2022-03-11 19:03:02 +0100

    No sé hacer un simple if :sad:

--- server/Game.gd
+++ server/Game.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 
 var controller_manager: ControllersManager
+const DISABLE_SEARCH = true # INFO: Set to false to connect controllers *while debugging*
 # var players = {}
-const DISABLE_SEARCH = true
 
 
 # Called when the node enters the scene tree for the first time.
@@ -33,9 +33,10 @@ func _ready():
 	GamePad.connect("gamepad_button_released", self, "_on_GamePad_button_released")
 
 	# Allow controllers to get connected
-	if OS.is_debug_build() and DISABLE_SEARCH:
+	if (not OS.has_feature("standalone")) and DISABLE_SEARCH:
 		print_debug("WARNING: Network controllers disabled while testing. Set DISABLE_SEARCH to " +
 		"False to modify this behaviour ")
+	else:
 		GamePad.search_for_controllers()
 
 func _on_GamePad_controller_connected(id):
