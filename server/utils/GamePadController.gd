e99b2cb48ca10dc5adce3cbf3668181304be9e13
Autotren <mail@uino.eu>
2022-03-11 19:27:16 +0100

    Añadida tecla dash

--- server/utils/GamePadController.gd
+++ server/utils/GamePadController.gd
@@ -8,7 +8,7 @@ func _init(controllersManager: ControllersManager, id: int):
 	_controllersManager = controllersManager
 	_id = id
 	#List of the current pressed state of the buttons of the gamepad. This is needed because the client sends the pressed/unpressed event 4 times for redundancy.
-	states = ["left", false, "right", false, "up", false, "down", false, "action", false]
+	states = ["left", false, "right", false, "up", false, "down", false, "action", false, "dash", false]
 
 func _to_string():
 	return (
@@ -17,6 +17,8 @@ func _to_string():
 func input(event:String, is_pressed:bool):
 	if event == "A":
 		event = "action"
+	if event == "B":
+		event = "dash"
 	var index = states.find(event)
 	if not bool(states[index + 1]) == is_pressed:
 		_controllersManager.controller_input(self, event, is_pressed)
