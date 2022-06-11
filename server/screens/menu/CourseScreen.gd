a790032711b3ae222e544ff6cd0a504e7dbe5485
yagueteiro <yagofreirefdez@gmail.com>
2022-06-11 15:38:19 +0200

    Añade soporte para teclas en menú de selección de nivel

--- server/screens/menu/CourseScreen.gd
+++ server/screens/menu/CourseScreen.gd
@@ -2,24 +2,27 @@ extends CanvasLayer
 
 
 var active_button = Vector2(0, 0)
+var columns = []
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Control/VBoxContainer/MarginContainer/HBoxContainer/VBoxContainer.get_children()[0].grab_focus()
-	pass # Replace with function body.
+	columns = $Control/VBoxContainer/MarginContainer/HBoxContainer.get_children()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	match active_button.x:
-		0.0:
-			if active_button.y > $Control/VBoxContainer/MarginContainer/HBoxContainer/VBoxContainer.get_child_count():
-				print("SI")
+func _process(_delta):
+	if len(columns) < active_button.x + 1:
+		active_button.x = 0
+	elif len(columns) < active_button.y + 1:
+		active_button.y = 0
+		
+	var container = columns[active_button.y]
+	container.get_children()[active_button.x].grab_focus()
+	print(active_button)
 
 
 func controller_input(_controller, action, is_main, _is_pressed):
-	print_debug("HJSRTJDHC")
 	if not _is_pressed:
 		return
 	if is_main:
@@ -28,6 +31,10 @@ func controller_input(_controller, action, is_main, _is_pressed):
 				active_button.x -= 1
 			"down":
 				active_button.x += 1
+			"left":
+				active_button.y -= 1
+			"right":
+				active_button.y += 1
 			"action":
 				$Control.get_children()[active_button].set_pressed(true)
 				$Control.get_children()[active_button].emit_signal("pressed")
